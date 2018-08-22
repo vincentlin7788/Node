f9f9653fd19f321ba722f3c16eadef73a1793322
vincentlin7788 <1459034@qq.com>
2018-08-22 12:29:13 +0800

    sf
    sss

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var logger = require('morgan');
 var mongo = require('mongodb');
 var monk = require('monk');
 //var db = monk('localhost:27017/nodetest2');
-var db = monk('mongodb://pigdb:Pighead123@ds161483.mlab.com:61483/mybot99/');
+var db = monk('mongodb://pigdb:dbpig778899@ds161483.mlab.com:61483/mybot99/');
 
 
 var indexRouter = require('./routes/index');
