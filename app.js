53a6e6c3984eb4970d26b1848dfb9bfc30a1ddb3
vincentlin7788 <1459034@qq.com>
2018-08-24 10:50:20 +0800

    df
    sss

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var logger = require('morgan');
 // Database ***********
 var mongo = require('mongodb');
 var monk = require('monk');
-//var db = monk('localhost:27017/nodetest2');
+//var db = monk('http://localhost:27017/nodetest2');
 var db = monk('mongodb://pigdb:dbpig778899@ds161483.mlab.com:61483/mybot99/');
 
 console.log("success");
