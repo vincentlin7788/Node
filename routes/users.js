70e5d862aa5931f6f9db50746dd7f78e8732a46b
vincentlin7788 <1459034@qq.com>
2018-08-24 10:44:59 +0800

    er
    ww

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET userlist. */
 router.get('/userlist', function(req, res) {
+	console.log('success of get');
   var db = req.db;
   var collection = db.get('userlist');
   collection.find({},{},function(e,docs){
