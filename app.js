095a4d6393b5154a07a09234128f174f660615fd
vincentlin7788 <1459034@qq.com>
2018-08-24 11:47:36 +0800

    d
    d

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var logger = require('morgan');
 // Database ***********
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('http://localhost:27017/nodetest2');
-//var db = monk('mongodb://pigdb:dbpig778899@ds161483.mlab.com:61483/mybot99/');
+//var db = monk('http://localhost:27017/nodetest2');
+var db = monk('mongodb://Admin:dbpig123@ds161483.mlab.com:61483/mybot99/');
 
 console.log("success");
 var indexRouter = require('./routes/index');
